5f476d8bae85f18a04451e56d05dbeb4844c0679
realSheng <panhe14@126.com>
2020-01-20 20:59:56 +0800

    update Cambricon Caffe code

--- scripts/travis/build.sh
+++ scripts/travis/build.sh
@@ -17,7 +17,7 @@ echo "Prepare swaps before build."
 free -m
 mkdir ./images
 rm -rf ./images/swap
-dd if=/dev/zero of=./images/swap bs=512 count=1M
+dd if=/dev/zero of=./images/swap bs=1024 count=1M
 sudo -E mkswap ./images/swap
 sudo -E swapon ./images/swap
 echo "After set swap space."
